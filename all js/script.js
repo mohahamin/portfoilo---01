30533c9e869b3b7f3732913f0ce5876ae33fca2f
mohahamin <70039956+mohahamin@users.noreply.github.com>
2021-04-04 00:53:12 +0200

    Update script.js

--- all js/script.js
+++ all js/script.js
@@ -4,7 +4,7 @@
           hour = minute * 60,
           day = hour * 24;
 
-    let birthday = "April 13, 2021 00:00:00",
+    let birthday = "April 13, 2021 00:55:00",
         countDown = new Date(birthday).getTime(),
         x = setInterval(function() {    
 
